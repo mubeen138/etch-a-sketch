3470043bdce0338312fce8a9cb9abcc2ed03ce07
mubeen138 <mubeen_138@hotmail.com>
2022-03-27 17:34:05 +0500

    Added CSS and full ability

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,12 @@ let containerDiv = document.querySelector('.container');
 let sideBar = document.querySelector('.side-bar');
 let gridDiv;
 let clearButton;
+let mouseDown = false;
 
+// Add mousedown and mouseup event listeners to container div
+containerDiv.addEventListener('mousedown',()=> mouseDown = true);
+containerDiv.addEventListener('mouseup',() => mouseDown = false);
+containerDiv.setAttribute('draggable', 'false');
 // Create Clear Button to clear and prompt
 
 clearButton = document.createElement('button');
@@ -18,8 +23,9 @@ function removeAllChildNodes(parent) {
 }
 sideBar.appendChild(clearButton);
 
+// Clear contents of pad and ask for new grid size
 function clearPad(){
-    let input = prompt("Enter Number of pixels per side (Max 100)");
+    let input = parseInt(prompt("Enter Number of pixels per side (Max 100)"));
     if (input > 100){
         clearPad();  
     }else{
@@ -27,13 +33,17 @@ function clearPad(){
         createGrid(input);
     }
 }
+//Create Grid
 function createGrid(numDivs = 16){
     for (i = 0; i < numDivs**2; i++){
         gridDiv = document.createElement('div');
         gridDiv.style.width = `${940/numDivs}px`; 
         gridDiv.style.height = `${940/numDivs}px`;
-        gridDiv.addEventListener('mousedown',e => {
-        e.target.style.backgroundColor = 'black';
+        gridDiv.setAttribute('draggable', 'false')
+        gridDiv.addEventListener('mouseenter',e => {
+            if(mouseDown){
+                e.target.style.backgroundColor = 'black';
+            }
         })
         gridDiv.classList.add('gridDiv');
         containerDiv.appendChild(gridDiv);
